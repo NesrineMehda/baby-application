452b5cd296a36b9937dac38f189a169194eed7ff
houda <nh.adjeneg@esi-sba.dz>
2024-05-08 23:39:58 +0100

    my third commit

--- baby_app_back/sign_up/urls.py
+++ baby_app_back/sign_up/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('',views.getUsers),
     path('create/', views.registration_view, name= 'create'),
+    path('login/',views.login, name='login'),
 
 ]
--- baby_app_back/sign_up/views.py
+++ baby_app_back/sign_up/views.py
@@ -1,28 +1,44 @@
-from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework.decorators import api_view
 from .models import User
 from rest_framework import status
+from django.contrib.auth.hashers import check_password
 
 
 @api_view(['GET'])
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
-@api_view(['POST',])
+@api_view(['POST'])
 def registration_view(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
         data = {}
         if serializer.is_valid():
-            user=serializer.save()
-            data['response']="successfully registrated"
-            data['email']=user.email
-            data['username']=user.username
-            return Response(data)
+            user = serializer.save()
+            data['response'] = "Successfully registered"
+            data['email'] = user.email
+            data['username'] = user.username
+            return Response(data, status=status.HTTP_201_CREATED)
         else:
             data = serializer.errors
-            return Response(data)
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        return Response({'error': 'Invalid username'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    if not check_password(password, user.password):
+        return Response({'error': 'Invalid password'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    # Authentication successful
+    return Response({'message': 'Login successful'}, status=status.HTTP_200_OK)
